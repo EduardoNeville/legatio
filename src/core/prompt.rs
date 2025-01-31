bbe8d2a0635c15420658a25cff9aafd072d42f2a
EduardoNeville <eduardoneville82@gmail.com>
2025-01-31 17:23:30 +0100

    feat(ui): No wrap needed

--- src/core/prompt.rs
+++ src/core/prompt.rs
@@ -162,20 +162,12 @@ pub fn prompt_chain(prompts: &[Prompt], prompt: &Prompt) -> Vec<Prompt> {
 pub fn format_prompt(p: &Prompt) -> (String, String) {
     let p_str = format!(
         " |- Prompt: {}",
-        if p.content.chars().count() < 40 {
-            p.content.replace('\n', " ").to_string()
-        } else {
-            p.content[0..40].replace('\n', " ").to_string()
-        },
+        p.content.replace('\n', " ").to_string()
     );
 
     let o_str = format!(
         " |  Output: {}",
-        if p.output.chars().count() < 40 {
-            p.output.replace('\n', " ").to_string()
-        } else {
-            p.output[0..40].replace('\n', " ").to_string()
-        }
+        p.output.replace('\n', " ").to_string()
     );
 
     (p_str, o_str)
@@ -184,20 +176,12 @@ pub fn format_prompt(p: &Prompt) -> (String, String) {
 pub fn format_prompt_depth(p: &Prompt, b_depth: &str) -> (String, String) {
     let p_str = format!(
         "{b_depth}> Prompt: {}",
-        if p.content.chars().count() < 40 {
-            p.content.replace('\n', " ").to_string()
-        } else {
-            p.content[0..40].replace('\n', " ").to_string()
-        },
+        p.content.replace('\n', " ").to_string()
     );
 
     let o_str = format!(
         "{b_depth}> Output: {}",
-        if p.output.chars().count() < 40 {
-            p.output.replace('\n', " ").to_string()
-        } else {
-            p.output[0..40].replace('\n', " ").to_string()
-        }
+        p.output.replace('\n', " ").to_string()
     );
 
     (p_str, o_str)
